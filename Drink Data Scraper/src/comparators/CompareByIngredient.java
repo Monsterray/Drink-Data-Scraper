f75e6eca164b8ba2a30114d32f40f7f7ae36035c
Monsterray <inkrpen@gmail.com>
2017-04-25 17:42:29 -0700

    Created comparator to make sorting by ingredient possible haven't actually implemented it yet

---
+++ Drink Data Scraper/src/comparators/CompareByIngredient.java
@@ -0,0 +1,35 @@
+/**
+ * 
+ */
+package comparators;
+
+import java.util.Comparator;
+
+import objects.Drink;
+
+/**
+ * @author Monsterray
+ *
+ */
+public class CompareByIngredient implements Comparator<Drink> {
+
+	/* (non-Javadoc)
+	 * @see java.util.Comparator#compare(java.lang.Object, java.lang.Object)
+	 */
+	@Override
+	public int compare(Drink d1, Drink d2) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+
+	/* (non-Javadoc)
+	 * @see java.lang.Object#equals(java.lang.Object)
+	 */
+	@Override
+	public boolean equals(Object obj) {
+		// TODO Auto-generated method stub
+		return super.equals(obj);
+	}
+
+}
